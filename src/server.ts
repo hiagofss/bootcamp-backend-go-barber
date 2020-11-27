1183115d3eb5a690fb42a82ccebf8efd2afb39e9
Hiago Souza <hiagofss98@gmail.com>
2020-11-26 22:06:49 -0300

    Add CORS in the project

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 
 import 'express-async-errors';
 
@@ -13,6 +14,7 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
