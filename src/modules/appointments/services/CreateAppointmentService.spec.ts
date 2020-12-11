68ae97b44048dd0b42dc5f1c89eea1e9cf3d60a6
Hiago Souza <hiagofss98@gmail.com>
2020-12-10 22:16:55 -0300

    Configure Jest and create first test

---
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -0,0 +1,24 @@
+import 'reflect-metadata';
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointmentService', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123456789',
+    });
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123456789');
+  });
+
+  // it('should not be able to create two appointments at the same time', () => {
+  //   const createAppointmentService = new CreateAppointmentService();
+  // });
+});
