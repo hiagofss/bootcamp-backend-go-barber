22fc2a7c13368ed1b68d18a843314e9b8a148d7a
Hiago Souza <hiagofss98@gmail.com>
2020-12-17 20:50:12 -0300

    Reset Password

--- src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
+++ src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -1,10 +1,10 @@
 import { v4 as uuid } from 'uuid';
 
-import IUserTokensRepository from '../IUserTokensRepository';
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
 
-import UserToken from '../../infra/typeorm/entities/User';
+import UserToken from '../../infra/typeorm/entities/UserToken';
 
-class UserTokensRepository implements IUserTokensRepository {
+class FakeUserTokensRepository implements IUserTokensRepository {
   private userTokens: UserToken[] = [];
 
   public async generate(user_id: string): Promise<UserToken> {
@@ -14,12 +14,22 @@ class UserTokensRepository implements IUserTokensRepository {
       id: uuid(),
       token: uuid(),
       user_id,
+      created_at: new Date(),
+      updated_at: new Date(),
     });
 
     this.userTokens.push(userToken);
 
     return userToken;
   }
+
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = this.userTokens.find(
+      findToken => findToken.token === token,
+    );
+
+    return userToken;
+  }
 }
 
-export default UserTokensRepository;
+export default FakeUserTokensRepository;
