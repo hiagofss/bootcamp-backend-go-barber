e226e5d60ff03eb68d57ef2107558f2074d8fe01
Hiago Souza <hiagofss98@gmail.com>
2020-12-30 23:49:05 -0300

    Upload files to Amazon S3

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,19 +1,49 @@
 import path from 'path';
 import crypto from 'crypto';
-import multer from 'multer';
+import multer, { StorageEngine } from 'multer';
 
 const tempFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
+interface IUploadConfig {
+  driver: 's3' | 'disk';
+
+  tempFolder: string;
+  uploadsFolder: string;
+
+  multer: {
+    storage: StorageEngine;
+  };
+
+  config: {
+    disk: {};
+    aws: {
+      bucket: string;
+    };
+  };
+}
+
 export default {
+  driver: process.env.STORAGE_DRIVER,
+
   tempFolder,
   uploadsFolder: path.resolve(tempFolder, 'uploads'),
-  storage: multer.diskStorage({
-    destination: tempFolder,
-    filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('hex');
-      const fileName = `${fileHash}-${file.originalname}`;
 
-      return callback(null, fileName);
+  multer: {
+    storage: multer.diskStorage({
+      destination: tempFolder,
+      filename(request, file, callback) {
+        const fileHash = crypto.randomBytes(10).toString('hex');
+        const fileName = `${fileHash}-${file.originalname}`;
+
+        return callback(null, fileName);
+      },
+    }),
+  },
+
+  config: {
+    disk: {},
+    aws: {
+      bucket: 'app-gobarber-app',
     },
-  }),
-};
+  },
+} as IUploadConfig;
