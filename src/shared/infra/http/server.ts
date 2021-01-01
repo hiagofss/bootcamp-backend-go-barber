e21bc6f686c8a8cc1e4e5f2dc7662baa3ac272f7
Hiago Souza <hiagofss98@gmail.com>
2021-01-01 17:59:05 -0300

    Apply rate limiter requests

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -10,6 +10,8 @@ import uploadConfig from '@config/upload';
 
 import AppError from '@shared/errors/AppError';
 
+import rateLimiter from './middlewares/rateLimiter';
+
 import routes from './routes';
 
 import '@shared/infra/typeorm';
@@ -17,6 +19,7 @@ import '@shared/container';
 
 const app = express();
 
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.uploadsFolder));
